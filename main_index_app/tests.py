94d3577fdc62ed3b8116f6f046e654bc1544a3d7
RaghavKeshari <rkesari@barytech.com>
2024-04-12 02:56:14 +0530

    Test Case added and requirements.txt updated for Rest API

--- main_index_app/tests.py
+++ main_index_app/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_prosperity_index.settings')
+django.setup()
+from ddf import G
+from .models import YearIndexTable
 
-# Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+from unittest.mock import patch
+
+class APITests(TestCase):
+    @patch('requests.get')
+
+    def test_get_index_Api(self, mock_get):
+        years = ["2014", "2015", "2016", "2017"]
+        for year in years:
+            G(YearIndexTable, year=year)
+        url = reverse("get_final_index")
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
